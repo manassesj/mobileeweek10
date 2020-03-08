2c0420040bead85f8f2e623099f38946e89b2cd2
Manassés júlio <jmanasses1024@hotmail.com>
2020-03-08 13:09:58 -0300

    Estilização e carregamento do mapa
    Estilização do Mark contido no mapa e o carregamento do posição inicial do usuario atraves do expo-location

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,89 @@
-import React from "react";
-import { View } from "react-native";
-import MapView from 'react-native-maps'
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Image, View, Text } from "react-native";
+import MapView, { Marker, Callout } from 'react-native-maps'
+import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location";
 
 function Main() {
-    return <MapView style={{ flex: 1  }} />
+
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialLocation() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
+            }
+        }
+
+        loadInitialLocation();
+
+    }, [])
+
+    if (!currentRegion) {
+        return null;
+    }
+
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map} >
+            <Marker coordinate={{ latitude: -8.0013192, longitude: -34.9755917 }} >
+                <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/34542065?s=460&v=4' }} />
+
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Diego Fernandes</Text>
+                        <Text style={styles.devBio}>Estudante de sistema de informação - UFRPE</Text>
+                        <Text style={styles.devTechs}>Reactjs, React Native</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+
+    );
 }
 
-export default Main;
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
 
-48 MIN https://drive.google.com/drive/folders/1-3B5qT5nuebzi42ook2HLl1ov09rhj8d
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff',
+    },
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5,
+    },  
+
+})
+
+export default Main;
