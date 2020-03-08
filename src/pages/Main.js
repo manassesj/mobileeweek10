f65e01d21b9aa80493be1bdaade6c41ee45c5877
Manassés júlio <jmanasses1024@hotmail.com>
2020-03-07 23:39:03 -0300

    configurações iniciais e o routes.js

---
+++ src/pages/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import MapView from 'react-native-maps'
+
+function Main() {
+    return <MapView style={{ flex: 1  }} />
+}
+
+export default Main;
+
+48 MIN https://drive.google.com/drive/folders/1-3B5qT5nuebzi42ook2HLl1ov09rhj8d
